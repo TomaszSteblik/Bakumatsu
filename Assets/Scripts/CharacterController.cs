39ab12abc863ba02cfa2c04b4c8240a6ab66ad9b
Rafał Kulka <68755164+rtrydev@users.noreply.github.com>
2022-01-14 17:12:45 +0100

    Controller fix (#17)
    * restore kulkas controllers
    
    * apply scene changes
    
    * add combat controls
    
    * add check whether an animation is playing
    
    * fix model boosting
    
    * fix collisions i guess
    
    * maybe fix movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Rigidbody _rigidbody;
-    public float speed = 10;
+    public float Speed = 0.1f;
+
+    public Vector3 movementVector;
+    public float RotationSpeed = 10f;
+
+    public GameObject Camera;
+
     private Animator _animator;
-    public Vector3 direction;
+    // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -18,31 +23,53 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = Vector3.zero;
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        direction = new Vector3(horizontal, 0, vertical);
-        if(Input.GetKeyDown(KeyCode.Q))
+
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("ATTACK_1"))
+            return;
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("ATTACK_2"))
+            return;
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("ATTACK_3"))
+            return;
+        
+        
+        if(Input.GetKeyDown(KeyCode.J))
             _animator.SetTrigger("ATTACK_1");
-        if(Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.K))
             _animator.SetTrigger("ATTACK_2");
-        if(Input.GetKeyDown(KeyCode.R))
+        if(Input.GetKeyDown(KeyCode.L))
             _animator.SetTrigger("ATTACK_3");
+
     }
 
     private void FixedUpdate()
     {
-        if (direction == Vector3.zero)
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("ATTACK_1"))
+            return;
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("ATTACK_2"))
+            return;
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("ATTACK_3"))
+            return;
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+        movementVector = new Vector3(horizontalAxis, 0f, verticalAxis);
+        movementVector = movementVector.normalized;
+
+        if (movementVector != Vector3.zero)
         {
-            _animator.SetFloat("Speed",0f);
+            movementVector = Quaternion.Euler(0f, Camera.transform.rotation.eulerAngles.y, 0f) * movementVector;
+            transform.position += movementVector * Speed * Time.deltaTime;
+
+
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(movementVector),
+                RotationSpeed * Time.deltaTime);
+            _animator.SetFloat("Speed",1f);
         }
         else
         {
-            transform.Translate(direction * speed * Time.deltaTime);
-            _animator.SetFloat("Speed",1f);
+            _animator.SetFloat("Speed",0f);
         }
-        
-        
-    }
 
+    }
 }
